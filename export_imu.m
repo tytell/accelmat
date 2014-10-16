5f8782291e2138c75801d4550cefc6cc19b8ea4f
Eric Tytell <eric.tytell@tufts.edu>
2014-10-16 10:28:33 -0400

    Now can merge EMG files and save as EDF

--- export_imu.m
+++ export_imu.m
@@ -1,7 +1,9 @@
 function export_imu(filename,calibfile, outfile, varargin)
 
-opt.resamplerate = 250;
+opt.resamplerate = [];
 opt.kinematicsfile = '';
+opt.emgfile = '';
+opt.outputformat = '';
 opt = parsevarargin(opt,varargin, 4);
 
 if nargin == 0
@@ -11,17 +13,25 @@ if nargin == 0
     [fn,pn] = uigetfile('*.mat','Choose calibration file',pn);
     calibfile = fullfile(pn,fn);
     
-    [fn,pn] = uigetfile('*.mat','Choose kinematics file (or Cancel for none)',pn);
-    if ~isempty(fn)
-        kinfile = fullfile(pn,fn);
+    [fn,pn1] = uigetfile('*.mat','Choose kinematics file (or Cancel for none)',pn);
+    if ischar(fn) && ~isempty(fn)
+        kinfile = fullfile(pn1,fn);
     else
         kinfile = '';
+    end
+    
+    [fn,pn1] = uigetfile('*.mat','Choose EMG file (or Cancel for none)',pn);
+    if ischar(fn) && ~isempty(fn)
+        emgfile = fullfile(pn1,fn);
+    else
+        emgfile = '';
     end
     
     [fn,pn] = uiputfile('*.txt','Choose output file',pn);
     outfile = fullfile(pn,fn);
 else
     kinfile = opt.kinematicsfile;
+    emgfile = opt.emgfile;
 end;
 
 if (ischar(calibfile) && exist(calibfile,'file'))
@@ -29,15 +39,50 @@ if (ischar(calibfile) && exist(calibfile,'file'))
 elseif isstruct(calibfile)
     calib = calibfile;
 end
-imu = load_imu(filename,calib);
+imu = load_imu(filename,calib,'resample',false);
+
+imurate = 1./(imu.t(2) - imu.t(1));
+resampletxt = sprintf('IMU=%.2fHz', imurate);
+defaultrate = imurate;
+
+if ~isempty(kinfile)
+    K = load(kinfile,'haxmmss','haymmss','humms','hvmms','hxmm',...
+        'hymm','txmm','tymm','t');
+    kinrate = 1/(K.t(2) - K.t(1));
+    resampletxt = [resampletxt sprintf(', Kinematics=%dHz', kinrate)];
+end
+
+if ~isempty(emgfile)
+    E = importLabChart(emgfile,'');
+    if ~isfield(E,'t')
+        error('Cannot handle multirate LabChart files yet...');
+    end
+    emgrate = E.samplerate(1);
+    resampletxt = [resampletxt sprintf(', EMG=%dHz', emgrate)];
+    defaultrate = emgrate;
+end
+
+if isempty(opt.resamplerate)
+    fprintf('Resample rate?\n  (%s) default=%d', resampletxt,defaultrate);
+    r = input(': ');
+    if ~isempty(r) && isnumeric(r)
+        resamplerate = r;
+    else
+        resamplerate = defaultrate;
+        fprintf('Resampling at %.2fHz.\n', defaultrate);
+    end
+else
+    resamplerate = opt.resamplerate;
+end
 
 imu = get_orient_imu(imu, 'getoffset');
 
 t0 = imu.t;
-t1 = (t0(1):1/opt.resamplerate:t0(end))';
+t1 = (t0(1):1/resamplerate:t0(end))';
 X0 = [imu.orient imu.acchi];
 X0(:,1:3) = X0(:,1:3) * 180/pi;     %convert to degrees
 X0(:,4:6) = X0(:,4:6) * 9.800;        %convert to m/s^2
+X1 = zeros(length(t1),6);
 for i = 1:size(X0,2)
     X1(:,i) = interp1(t0,X0(:,i), t1);
 end
@@ -46,55 +91,125 @@ X1 = [t1 X1];
 chantitle = {'Pitch','Roll','Yaw','UpAcc','FwdAcc','SideAcc'};
 tplt = '%.5f\t%.3f\t%.3f\t%.3f\t%.5f\t%.5f\t%.5f';
 if ~isempty(kinfile)
-    K = load(kinfile,'haxmmss','haymmss','humms','hvmms','hxmm',...
-        'hymm','txmm','tymm','t');
-    
     X2 = [K.hxmm' K.hymm' K.txmm' K.tymm' K.humms' K.hvmms' K.haxmmss' K.haymmss'];
-    K.t = K.t(end) - K.t;
+    K.t = K.t - K.t(end);
+    Xkin = zeros(length(t1),8);
     for i = 1:size(X2,2)
-        X3(:,i) = interp1(K.t,X2(:,i), t1);
+        Xkin(:,i) = interp1(K.t,X2(:,i), t1);
     end
-    X3 = X3 / 1000;       % convert to m
+    Xkin = Xkin / 1000;       % convert to m
     
-    X1 = [X1 X3];
+    X1 = [X1 Xkin];
     
     chantitle = [chantitle, {'FwdPos','SidePos','TailFwd','TailSide','FwdVel','SideVel',...
         'FwdAccVid','SideAccVid'}];
     tplt = [tplt '\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.5f\t%.5f'];
 end
 
-fid = fopen(outfile,'w');
-titletplt = ['ChannelTitle=\t' repmat('%s\t',[1 length(chantitle)-1]) '%s\n'];
-fprintf(fid, titletplt, chantitle{:});
-fprintf(fid, [tplt '\n'], X1');
-fclose(fid);
+annote.event = {};
+annote.starttime = [];
+annote.duration = [];
+
+if ~isempty(emgfile)
+    emgchantitle = fieldnames(E);
+    trigchanind = find(ismember(lower(emgchantitle),{'trigger','trig'}));
+    if ~isempty(trigchanind)
+        trigchan = emgchantitle{trigchanind};
+        t0 = first(E.t, E.(trigchan) < 3);
+    else
+        t0 = E.t(end);
+    end
+    
+    emgt = E.t - t0;
+    
+    if isfield(E,'commenttxt')
+        annote.event = E.commenttxt;
+        annote.starttime = E.commentt;
+        annote.duration = zeros(size(E.commentt));
+    end
+
+    len0 = length(E.t);
+    E = struct2cell(E);
+    len = cellfun(@length,E);
+    
+    ischan = (len == len0) & ~ismember(emgchantitle,{'t'});
+    if ~isempty(trigchanind)
+        ischan(trigchanind) = false;
+    end
+    X2 = cat(2,E{ischan});
+    
+    Xemg = zeros(length(t1),sum(ischan));
+    for i = 1:size(X2,2)
+        Xemg(:,i) = interp1(emgt,X2(:,i), t1);
+    end
+    
+    X1 = [X1 Xemg];
+    
+    emgchantitle = emgchantitle(ischan);
+    chantitle = [chantitle emgchantitle(:)'];
+    tplt = [tplt repmat('\t%.5f',[1 sum(ischan)])];
+    
+    
+end
+
+if isempty(opt.outputformat)
+    fprintf('Output file format:\n');
+    fprintf(' T. LabChart Text\n M. Matlab\n E. EDF\n A. All (default)\n');
+    fmt1 = input('Choose format: ','s');
+    
+    switch lower(fmt1)
+        case {'t','text'}
+            fmt = {'text'};
+        case {'m','mat','matlab'}
+            fmt = {'mat'};
+        case {'e','edf'}
+            fmt = {'edf'};
+        case {'a','all'}
+            fmt = {'text','mat','edf'};
+        otherwise
+            fmt = {'text','mat','edf'};
+    end
+else
+    fmt = lower(opt.outputformat);
+    if ~iscell(fmt)
+        fmt = {fmt};
+    end
+    if any(~ismember(fmt,{'text','mat','edf'}))
+        error('Unrecognized output format');
+    end
+end
+
+if ismember('text',fmt)
+    fid = fopen(outfile,'w');
+    titletplt = ['ChannelTitle=\t' repmat('%s\t',[1 length(chantitle)-1]) '%s\n'];
+    fprintf(fid, titletplt, chantitle{:});
+    fprintf(fid, [tplt '\n'], X1');
+    fclose(fid);
+end
 
 [pn,fn,ext] = fileparts(outfile);
-outmatfile = fullfile(pn,[fn '.mat']);
 
-t = t0;
-pitch = X0(:,1);
-roll = X0(:,2);
-yaw = X0(:,3);
-accup = X0(:,4);
-accfwd = X0(:,5);
-accside = X0(:,6);
-if exist('X3','var')
-    fwdpos = X3(:,1);
-    sidepos = X3(:,2);
-    tailfwd = X3(:,3);
-    tailside = X3(:,4);
-    fwdvel = X3(:,5);
-    sidevel = X3(:,6);
-    fwdaccvid = X3(:,7);
-    sideaccvid = X3(:,8);
+if ismember('mat',fmt)
+    outmatfile = fullfile(pn,[fn '.mat']);
     
-    extravars = {'fwdpos','sidepos','tailfwd','tailside','fwdvel','sidevel',...
-        'fwdaccvid','sideaccvid'};
-else
-    extravars = {};
+    C = mat2cell(X1, length(t1), ones(1,size(X1,2)));
+    S = cell2struct(C(:),['t' chantitle],1);
+    save(outmatfile,'-struct','S');
 end
-save(outmatfile,'t','pitch','roll','yaw','accup','accfwd','accside', extravars{:});
+
+if ismember('edf',fmt)
+    outedffile = fullfile(pn,[fn '.edf']);
+    
+    header.samplerate = resamplerate;
+    header.duration = length(t1)/resamplerate;
+    header.labels = chantitle;
+    header.annotation = annote;
+    SaveEDF(outedffile,X1(:,2:end),header);
+end
+
+    
+            
+        
 
 
 
