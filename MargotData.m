a81afb3702d1f733b9e973244fb9bb62db05301d
Eric Tytell <Eric.Tytell@tufts.edu>
2015-05-08 10:09:21 -0400

    Minor changes to Vishesh's code so that I can run it easily.

--- MargotData.m
+++ MargotData.m
@@ -1,7 +1,7 @@
 % Load data
 function MargotData()
 
-theNoise01      = load_imu2('noisytest001.h5');
+theNoise01      = load_imu('rawdata/bg14/Accelerometer/noisytest001.h5');
 theNoise01.gyro = deg2rad(theNoise01.gyro);
 Qgyro           = cov(theNoise01.gyro(1.5e4:8e4,:));
 constBiasGyro   = mean(theNoise01.gyro(1.5e4:8e4,:),1);
@@ -11,11 +11,16 @@ Qdyn            = 10*Qacc;
 
 Qbias           = zeros(3,3);
 
-% theData         =  load_imu2('bg14_004.h5');
-theData         = load('Bg5-20_0hz-005.mat');
+[~,acccalib] = load_imu('rawdata/bg14/Accelerometer/calib001.h5',[],'calib','axisorder',{'Y','-Z','-X'});
+
+theData         =  load_imu('rawdata/bg14/Accelerometer/bg14_004.h5');
+%theData         = load('Bg5-20_0hz-005.mat');
 theData.gyro = deg2rad(theData.gyro);
 % keyboard
 % [results]       = smootherRTS2(theData, Qgyro, Qbias, Qacc);
 % results         = smootherRTSwithAv3(theData, Qgyro, Qbias, Qacc, 10*Qdyn, -0.75, 10);
-results         = ERTSSv1(theData, Qgyro, Qbias, Qacc, Qdyn, 1.1, 10);
-end
+
+knownyaw = [1 length(theData.gyro)];
+theData.realeulerrad = zeros(size(theData.gyro'));
+
+results         = ERTSSv1(theData, Qgyro, Qbias, Qacc, Qdyn, 1.1, knownyaw);
