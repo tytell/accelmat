3f1bc668c6aef00599c0553187ed7819dbe44b74
Eric Tytell <Eric.Tytell@tufts.edu>
2018-09-27 17:00:49 -0400

    Added some more details about loading EMG data also

--- how_to_load_imu_data.m
+++ how_to_load_imu_data.m
@@ -7,10 +7,23 @@ imu = load_imu('path/to/data.h5', calib, ...
     'timerange', [-5 1], ...        % just load from -5 to 1 sec, rel to trigger
     'resamplerate',100);
 
-h(1) = subplot(2,1,1);
+h(1) = subplot(3,1,1);
 plot(imu.t, imu.gyro);
 
-h(2) = subplot(2,1,2);
+h(2) = subplot(3,1,2);
 plot(imu.t, imu.acc);
+
+
+%%% Loading LabChart daat
+% Export the relevant part of your data set as a Matlab file from LabChart
+
+importLabChart('/path/to/exported/LabChart/file.mat', ...
+    '/path/to/better/matfile.mat');
+
+emg = load('/path/to/better/matfile.mat');
+
+h(3) = subplot(3,1,3);
+t0 = 10;        % find the time for the trigger from the EMG data
+plot(emg.t - t0, emg.channelname1);     % where channelname1 is the channel name from LabChart
 
 linkaxes(h, 'x');
